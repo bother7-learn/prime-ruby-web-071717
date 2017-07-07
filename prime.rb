5c7868e46cb97d5bcad927fcda756c154d46394f
Joe Cha <josephmcha@gmail.com>
2017-07-07 14:51:21 -0400

    Done

--- prime.rb
+++ prime.rb
@@ -1,1 +1,21 @@
-# Add  code here!
+
+def prime?(num)
+  if num > 1
+    count = 0
+array = []
+    while count < num do
+      array[count] = count
+      count += 1
+    end
+    array.shift
+    array.shift
+    shift = array.collect {|mod| num % mod}
+    !shift.include?(0)
+  else
+    false
+  end
+
+
+
+  # Add  code here!
+end
